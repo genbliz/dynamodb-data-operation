2350c4204aa253932de35f897701a14c32b8170d
Chris Uzoh <genbliz@gmail.com>
2020-07-10 11:43:51 +0100

    exposed some type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { DynamoDataOperation } from "./core/dynamo-data-operation";
 export { ICoreEntityBaseModel } from "./core/base-schema";
 export { GenericDataError } from "./helpers/errors";
+export { ISecondaryIndexDef } from "./types/base-types";
 
 export default DynamoDataOperation;
